391f3f19e113f3fbe620a070c832d3d8b6af28e2
satyam-pokee <satyam-pokee@gmail.com>
2023-09-09 18:07:26 +0530

    initial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BuyComponentComponent } from './buy-component/buy-component.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/buy-component/buy-component.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-buy-component',
+  templateUrl: './buy-component.component.html',
+  styleUrls: ['./buy-component.component.scss']
+})
+export class BuyComponentComponent implements OnInit {
+  selectedCountry!: string;
+  countries!: any[]; // Array to store the list of countries and currencies
+
+  constructor(private http: HttpClient) {
+    // Fetch the JSON data from the assets folder
+    this.http.get<any[]>('/assets/countries.json').subscribe(data => {
+      this.countries = data;
+    });
+  }
+  ngOnInit(): void {
+  }
+  activeTab: string = 'buy'; // Initialize the active tab to 'buy'
+  selectedCurrency: string = 'USD';
+  amount: number = 0;
+  exchangeRate: number = 1.2; // Example exchange rate
+
+  buy(): void {
+    // Implement the buy logic here
+  }
+}
